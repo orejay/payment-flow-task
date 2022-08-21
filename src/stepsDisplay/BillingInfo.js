ffe438b80c3f64e20138e87098c2205e5cfc708e
Ore Jaiyesimi <orejay2017@gmail.com>
2022-08-21 17:23:03 +0100

    build

--- src/stepsDisplay/BillingInfo.js
+++ src/stepsDisplay/BillingInfo.js
@@ -15,7 +15,7 @@ export default function BillingInfo() {
         <div className='form'>
             <div className='input-wrap'>
                 <label htmlFor="name">Name on Card <sup>*</sup></label>
-                <input type="text" name='name' id='card-name' className='required' required/>
+                <input type="text" name='name' id='card-name' className='required' placeholder='Opara Linus Ahmed' required/>
             </div>
             <div className='input-wrap'>
                 <label htmlFor="name">Card Type <sup>*</sup></label>
@@ -28,15 +28,15 @@ export default function BillingInfo() {
             <div className='input-wrap-row'>
                 <div className='input-wrap' id='card-bx'>
                     <label htmlFor="lg">Card details <sup>*</sup></label>
-                    <InputMask mask="99999   99999   99999   99999" className='required' required/>
+                    <InputMask mask="99999   99999   99999   99999" className='required' placeholder='44960   44960   44960   44960' required/>
                 </div>
                 <div className='input-wrap' id='expiry-bx'>
                     <label htmlFor="expiry">Expiry date <sup>*</sup></label>
-                    <InputMask mask={mask} formatChars={formatChars} className='required' required/>
+                    <InputMask mask={mask} formatChars={formatChars} className='required' placeholder='04/23' required/>
                 </div>
                 <div className='input-wrap' id='cvv-bx'>
                     <label htmlFor="state">CVV <sup>*</sup></label>
-                    <InputMask mask="999" id='cvv' className='required' required/>
+                    <InputMask mask="999" id='cvv' className='required' placeholder='923' required/>
                 </div>
             </div>
         </div>
--- src/stepsDisplay/PersonalInfo.js
+++ src/stepsDisplay/PersonalInfo.js
@@ -6,25 +6,25 @@ export default function PersonalInfo() {
     <div className='form'>
       <div className='input-wrap'>
         <label htmlFor="name">Name</label>
-        <input type="text" name='name' id='name' />
+        <input type="text" name='name' id='name' placeholder='Opara Linus Ahmed'/>
       </div>
       <div className='input-wrap'>
         <label htmlFor="name">Email Address <sup>*</sup></label>
         <p id='email-desc'>The purchase reciept would be sent to this address</p>
-        <input type="email" name='email' id='email' className='required' required/>
+        <input type="email" name='email' id='email' className='required' placeholder='OparaLinusAhmed@devmail.com' required/>
       </div>
       <div className='input-wrap'>
         <label htmlFor="address-1">Address 1</label>
-        <input type="address" name='address-1' id='address-1' />
+        <input type="address" name='address-1' id='address-1' placeholder='The address of the user goes here'/>
       </div>
       <div className='input-wrap'>
         <label htmlFor="name">Address 2</label>
-        <input type="address" name='address-2' id='address-2' />
+        <input type="address" name='address-2' id='address-2' placeholder='and here' />
       </div>
       <div className='input-wrap-row'>
         <div className='input-wrap' id='lg-bx'>
             <label htmlFor="lg">Local Government</label>
-            <input type="text" name='lg' id='lg' />
+            <input type="text" name='lg' id='lg' placeholder='Surulere' />
         </div>
         <div className='input-wrap' id='state-bx'>
             <label htmlFor="state">State</label>
